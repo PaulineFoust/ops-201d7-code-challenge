dda7fcab64a16421048311a61fb6dacf978b6c44
PaulineFoust <misspaulinefoust@gmail.com>
2023-04-26 22:17:52 -0400

    firstpush

---
+++ 201OC3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 03 Ops Challenge Solution
+# Author: Pauline Foust
+# Date of latest revision: 4/26/23
+# Purpose: Print a string to the terminal
+
+sudo last /var/log/wtmp
+
+echo "This is the login history."
+
+sudo last -3
+
+
