5fe2d028ad61661004f076b32b09511a3fcaf8b6
PaulineFoust <misspaulinefoust@gmail.com>
2023-05-08 21:13:59 -0400

    Windows Batch Scripting

---
+++ 201OC8.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 08 Ops Challenge Solution
+# Author: Pauline Foust
+# Date of latest revision: 5/08/23
+# Purpose: Windows Batch Scripting
+
+@echo off
+ROBOCOPY  "C:\Users\j.thompson\Documents" "C:" /e
+pause
+
+#end
