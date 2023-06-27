0154a5a5f6638568b7d5183e55a91519eb879516
PaulineFoust <misspaulinefoust@gmail.com>
2023-06-26 21:43:11 -0400

    first push

--- 301OC6.py
+++ 301OC6.py
@@ -3,17 +3,35 @@
 # Date of latest revision: 6/19/23
 # Purpose: Python Exercise
 
+# Objectives
+# Before proceeding, ensure that course prework assignment “Setup Python” is complete.
+# Microsoft Visual Studio Code is the recommended IDE for Python assignments throughout the Ops sequence.
+# In Ubuntu, create a Python script that executes a few bash commands successfully. Indicate in comments how you achieved this.
+# Requirements:
+# The Python module “os” must be utilized
+# At least three variables must be declared in Python that contain results of bash operations
+# The Python function print() must be used at least three times
+# Include execution of the following bash commands inside your Python script:
+# -whoami
+# -ip a
+# -lshw -short
+
 import os
 
+# Execute 'whoami' command and store the output in a variable
 x = os.popen('whoami').read()
 
+# Execute 'ip a' command and store the output in a variable
 y = os.popen('ip a').read()
 
+# Execute 'lshw -short' command and store the output in a variable
 z = os.popen('lshw -short').read()
 
-
+# Print the result of 'whoami' command
 print(x)
 
+# Print the result of 'ip a' command
 print(y)
 
+# Print the result of 'lshw -short' command
 print(z)
---
+++ dir1/301OC8.py
@@ -0,0 +1,25 @@
+# Script: Ops 301 Class 05 Ops Challenge Solution
+# Author: Pauline Foust
+# Date of latest revision: 6/14/23
+
+# Purpose: Python Exercise# Objectives
+# Create a Python script that includes the following:
+# Assign to a variable a list of ten string elements.
+# Print the fourth element of the list.
+# Print the sixth through tenth element of the list.
+# Change the value of the seventh element to “onion”.
+
+# Assign to a variable a list of ten string elements.
+myList = ['apple', 'banana', 'grapes', 'cherry', 'kiwi', 'lemon','watermelon', 'apricots', 'pineapple', 'orange']
+
+# Print the fourth element of the list.
+fourthElement = myList[3]
+print(fourthElement)
+
+# Print the sixth through tenth element of the list.
+sixElement = myList[5]
+print(sixElement)
+
+# Change the value of the seventh element to “onion”.
+myList[6] = 'onion'
+print(myList)
